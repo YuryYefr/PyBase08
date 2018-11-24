ace7c22721c884d1d0817f43a72583cad2e09ab0
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-11-24 16:23:31 +0200

    Add files via upload

---
+++ HW_1.py
@@ -0,0 +1,41 @@
+#Запрос имени и фамилии пользователя
+name = input("what is your name?: ").title()
+surname = input("what is your surname?:").title()
+print(f"Hello {name} {surname}!")
+#Дата рождения
+year = int(input("what year you were born?: "))
+day = int(input("day: "))
+month = int(input("month:  "))
+user_input_y = 2018 - year
+user_input_m = user_input_y * 12 + (11 - month)
+user_input_d = 349 - ((month - 1) * 30 + day) + user_input_y * 360
+print(f"You are {user_input_y} years old or {user_input_m} months old")
+#Прожито до начала курса 19.11.2018
+print(f"you lived before course start {user_input_d} days, {user_input_m} months, {user_input_y} years!")
+print("HW_2")
+
+
+def some_operations(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    print(a % b)
+    print(a // b)
+    print(a ** b)
+
+
+a = int(input("input number: "))
+b = int(input("input number: "))
+some_operations(a, b)
+print("HW 3")
+a = int(input("input number: "))
+print(int(a), float(a), bool(a), str(a))
+b = int(input("input number: "))
+print(int(a) + int(b), float(a) + float(b), bool(a) + bool(b), str(a) + str(b))
+print(int(a) * int(a), float(a) * float(b), bool(a) * bool(b))
+
+
+
+
+
