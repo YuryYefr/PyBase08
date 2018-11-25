4aa26c4e19706eba40f1f19560d2a9a97bb4779a
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-11-25 09:43:20 +0200

    Add files via upload

---
+++ HW_1_2.py
@@ -0,0 +1,14 @@
+Print("Операции над числами")
+def some_operations(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    print(a % b)
+    print(a // b)
+    print(a ** b)
+
+
+a = int(input("input number: "))
+b = int(input("input number: "))
+some_operations(a, b)
---
+++ HW_1_3.py
@@ -0,0 +1,7 @@
+Print("Операции + и *")
+a = int(input("input number: "))
+print(int(a), float(a), bool(a), str(a))
+b = int(input("input number: "))
+print(int(a) + int(b), float(a) + float(b), bool(a) + bool(b), str(a) + str(b))
+print(int(a) * int(a), float(a) * float(b), bool(a) * bool(b))
+
