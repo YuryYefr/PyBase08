87acb32638b464144f1c8aaed81833c60579876e
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-12-02 17:50:42 +0200

    Delete HW_2_3.py

--- HW_2_3.py
+++
@@ -1,9 +0,0 @@
-a = input("Факториал числа ")
-while a.isalpha():
-    a = input("Введите число ")
-factorial = 1
-i = 0
-while i < int(a):
-     i += 1
-     factorial = factorial * i
-print("ваше число", factorial)
