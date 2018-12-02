c4fb6cbb5b230eb5b394aac061e4c302d44c85fa
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-12-02 17:51:24 +0200

    Add files via upload

---
+++ HW_2_3.py
@@ -0,0 +1,10 @@
+#Факториал
+a = input("Факториал числа ")
+while a.isalpha() or int(a) < 0:
+    a = input("Введите число ")
+factorial = 1
+i = 0
+while i < int(a):
+     i += 1
+     factorial = factorial * i
+print("ваше число", factorial)
