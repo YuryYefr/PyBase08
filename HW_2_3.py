e56c7f06aaac2983c793e71b6e36836f7822c131
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-12-02 17:58:33 +0200

    Update HW_2_3.py

--- HW_2_3.py
+++ HW_2_3.py
@@ -1,7 +1,7 @@
 #Факториал
 a = input("Факториал числа ")
 while a.isalpha() or int(a) < 0:
-    a = input("Введите число ")
+    a = input("Введите целое число ")
 factorial = 1
 i = 0
 while i < int(a):
