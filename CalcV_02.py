0dddd02f7171b337b68a41aa6fd27c13b21162da
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-12-13 22:22:01 +0200

    Add files via upload

---
+++ CalcV_02.py
@@ -0,0 +1,69 @@
+import math
+
+
+def add(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a- b
+
+
+def division(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("You cannot divide on zero")
+
+
+def multiply(a, b):
+    return a * b
+
+
+def sqrt(a):
+    return math.sqrt(a)
+
+
+def logic():
+    a = input("Number?: ")
+    while a.isspace() or a.isalpha():
+        print("there is something wrong with your input, please try again")
+        a = input("Number?: ")
+    user_choice1 = float(a)
+    b = input("What is second number?: ")
+    while a.isspace() or a.isalpha():
+        print("there is something wrong with your input, please try again")
+        b = input("What is second number?: ")
+    user_choice2 = float(b)
+    print("\n1.if you want to add"
+          "\n2.if you want to minus"
+          "\n3. if you want to divide"
+          "\n4. if you want to multiply")
+    user_operations = input("you choose what?:")
+    while user_operations.isspace() or user_operations.isalpha():
+        print("there is something wrong with your input, please try again")
+        user_operations = input("you choose what?: ")
+    if user_operations == "1":
+        return add(user_choice1, user_choice2)
+    elif user_operations == "2":
+        return minus(user_choice1, user_choice2)
+    elif user_operations == "4":
+        return multiply(user_choice1, user_choice2)
+    elif user_operations == "3":
+        return division(user_choice1, user_choice2)
+    else:
+        print("something wrong with your choice")
+
+
+if __name__ == "__main__":
+    userName = " "
+    print("You must be logged on to use the application\nWould you like to register?")
+    user_entry = input("Y/N:")
+    if user_entry.lower() == "n" or user_entry.isspace():
+        print("program is shutting down")
+        exit()
+    elif user_entry.lower() == "y":
+        userName = input("what is your name?:")
+    print(f"Welcome {userName.capitalize()}\tlet's do some calculate")
+c = logic()
+print(c)
