10f1b42941a70a315323a7bc1dec089fe6e6634b
Dos <43720200+YuryYefr@users.noreply.github.com>
2018-12-18 09:35:33 +0200

    Add files via upload

---
+++ HW_4_01.py
@@ -0,0 +1,23 @@
+from string import ascii_letters, digits, whitespace, punctuation
+
+
+def clear_word(word, filterstr):
+    result = ""
+    for element in word.split():
+        if element in filterstr:
+            result = word.replace(element, "")
+    for element in word[::]:
+        if element in filterstr:
+            raise ValueError(word.index(element))
+
+    return result
+
+
+if __name__ == "__main__":
+    word = "\tShakespeare's sonnets are poems" \
+           "\n that William Shakespeare wrote on a variety of themes." \
+ \
+           "\n When discussing or referring to Shakespeare’s sonnets, it is almost always a reference to the 154 sonnets"
+    filterstr = ascii_letters, digits, whitespace, punctuation
+    res = clear_word(word, filterstr)
+    print(res)
